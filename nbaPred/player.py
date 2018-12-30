af2963f7a3abe39f328bd7e403102993baf90547
Alejandro Cegarra <cegarraa@berkeley.edu>
2018-12-29 20:36:03 -0500

    started implementing per and stat aggregation over seasons

--- nbaPred/player.py
+++ nbaPred/player.py
@@ -26,17 +26,22 @@ class Player:
         made_three_point_field_goals: (int)
         personal_fouls: (int)
         blocks: (int)
+        end_year: (int)
     """
-    def __init__(self, player_dict, year):
+    def __init__(self, player_dict, year, start_date=None, end_date=None):
         """
         Args:
             player_dict: (dict)
             year: (int)
         """
+        self.player_dict = player_dict
         utils.validate_player_dict(player_dict)
         for key in player_dict:
             setattr(self, key, player_dict[key])
         self.end_year = year
+        self.points = 3*self.made_three_point_field_goals + \
+            2* (self.made_field_goals - self.made_three_point_field_goals) \
+            + self.made_free_throws
 
     def __str__(self):
         return "<%s, %s, %d>" % (self.name, self.team, self.end_year)
@@ -51,8 +56,44 @@ class Player:
             players.append(Player(player_dict, end_year))
         return players
 
+    def per(self):
+        return self.easy_per()
+
+    def easy_per(self):
+        team = utils.season_team_totals(self.end_year, self.team)
+        league = utils.season_league_totals(self.end_year)
+        team_ast = team["assists"]
+        team_fg = team["attempted_field_goals"]
+        lg_PTS = league["points"]
+        lg_FGA = league["attempted_field_goals"]
+        lg_ORB = league["offensive_rebounds"]
+        lg_TOV = league["turnovers"]
+        lg_FT = league["made_free_throws"]
+        lg_FG = league["made_field_goals"]
+        lg_FTA = league["attempted_free_throws"]
+        lg_PF = league["personal_fouls"]
+        lg_AST = league["assists"]
+        factor = (2 / 3) - (0.5 * (lg_AST / lg_FG)) / (2 * (lg_FG / lg_FT))
+        VOP = lg_PTS / (lg_FGA - lg_ORB + lg_TOV + 0.44 * lg_FTA)
+        TOV = self.turnovers
+        DRB_ratio = league["defensive_rebounds"] / (league["defensive_rebounds"] + league["offensive_rebounds"]) 
+        return (1/ self.minutes_played) \
+            * (self.made_three_point_field_goals \
+            + (2/3)*self.assists \
+            + (2 - factor*(team_ast/team_fg)) * self.attempted_field_goals \
+            + (self.made_free_throws*.5*(1 + (1- (team_ast/team_fg)))+(2/3)*(team_ast/team_fg)) \
+            - VOP * TOV \
+            - VOP * DRB_ratio * (self.attempted_field_goals - self.made_field_goals) \
+            - VOP * .44 * (.44 + (.56*DRB_ratio)) * (self.attempted_field_goals - self.made_field_goals) \
+            + VOP * (1 - DRB_ratio)*self.defensive_rebounds \
+            + VOP * DRB_ratio * self.offensive_rebounds \
+            + VOP * self.steals \
+            + VOP * DRB_ratio * self.blocks \
+            - self.personal_fouls * ((lg_FT / lg_FG) - .44 * (lg_FTA / lg_PF) * VOP))
+
+
 if  __name__ == "__main__":
     players = Player.get_players(2018)
     for p in players:
-        print(p)
+        print(p.name, p.easy_per())
 
--- nbaPred/utils.py
+++ nbaPred/utils.py
@@ -1,5 +1,10 @@
 from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
 PLAYER_KEYS = {'turnovers', 'attempted_free_throws', 'age', 'games_started', 'assists', 'minutes_played', 'team', 'positions', 'steals', 'offensive_rebounds', 'made_free_throws', 'games_played', 'attempted_field_goals', 'defensive_rebounds', 'made_field_goals', 'name', 'attempted_three_point_field_goals', 'made_three_point_field_goals', 'personal_fouls', 'blocks'}
+import os, json, datetime
+from datetime import date
+SEASONS = {2018: (date(2017, 10, 18), date(2018, 6, 7))
+            }
 
 def validate_player_dict(player_dict):
     """
@@ -43,7 +48,12 @@ def get_player_totals(end_year):
     Returns:
         (list<dict>) a list of player dictionaries
     """
-    return client.players_season_totals(end_year)
+    path_to_json = "../data/players/%s_players.json" % str(end_year)
+    if not os.path.exists(path_to_json):
+        client.players_season_totals(end_year, output_type=OutputType.JSON,
+            output_file_path=path_to_json)
+    f = open(path_to_json, "r")
+    return json.load(f)
 
 def get_games(end_year):
     """
@@ -51,4 +61,49 @@ def get_games(end_year):
         end_year: (int)
     Returns:
     """
-    return client.season_schedule(end_year)
+    path_to_json = "../data/games/%s_games.json" % str(end_year)
+    if not os.path.exists(path_to_json):
+        client.season_schedule(end_year, output_type=OutputType.JSON,
+            output_file_path=path_to_json)
+    f = open(path_to_json, "r")
+    return json.load(f)
+
+def prune(player_dict):
+    prune_list = ["age", "games_started", "team", "positions", "name"]
+    for key in prune_list:
+        player_dict.pop(key)
+
+def season_league_totals(end_year):
+    player_dicts = get_player_totals(end_year)
+    return get_totals(player_dicts)
+
+def season_team_totals(end_year, team):
+    player_dicts = get_player_totals(end_year)
+    player_dicts = [player for player in player_dicts if player["team"] == team]
+    return get_totals(player_dicts)
+
+def get_totals(player_dicts):
+    totals = player_dicts[0]
+    prune(totals)
+    for player_dict in player_dicts[1:]:
+        prune(player_dict)
+        for key in totals.keys():
+            assert type(totals[key] in (float, int))
+            totals[key] += player_dict[key]
+    add_points(totals)
+    return totals 
+
+def add_points(player_dict):
+    player_dict["points"] = 3*player_dict["made_three_point_field_goals"] + \
+            2* (player_dict["made_field_goals"] - player_dict["made_three_point_field_goals"]) \
+            + player_dict["made_free_throws"]
+
+"""
+def range_global_totals(start_date, end_date):
+"""
+
+def date_range(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
